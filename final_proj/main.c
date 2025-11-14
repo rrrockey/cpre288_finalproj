75a2ea8ca09223262938f7f190ff4e8c8e6936d5
Rockey <rockey@iastate.edu>
2025-11-13 19:14:04 -0600

    feat: add forward movement calibration, may or may not use these features if we use the depth sensor

--- final_proj/main.c
+++ final_proj/main.c
@@ -1,3 +1,8 @@
+#include "open_interface.h"
+#include "movement.h"
+#include "Timer.h"
+
+
 
 
 /**
@@ -5,5 +10,15 @@
  */
 int main(void)
 {
+    timer_init();
+    lcd_init();
+    oi_t *sensor_data = oi_alloc();
+    oi_init(sensor_data);
+
+    calibrate_forward_movement(sensor_data);
+
+    oi_free(sensor_data);
+
+    while (1);
 	return 0;
 }
