2307544111cfa288098ff69f768df30c94615607
Rockey <rockey@iastate.edu>
2025-11-20 18:53:25 -0600

    make changes

--- final_proj/main.c
+++ final_proj/main.c
@@ -22,6 +22,136 @@ typedef struct {
     double width;
 } object_t;
 
+typedef struct {
+    int averagePing;
+    double averageAdc;
+} scan_info;
+
+#define POSITIVE_Y 0
+#define POSITIVE_X 1
+#define NEGATIVE_Y 2
+#define NEGATIVE_X 3
+int facing_array[4] = {POSITIVE_Y, POSITIVE_X, NEGATIVE_Y, NEGATIVE_X};
+
+scan_info scan_cone(int low, int high){
+    scan_info scanData = {0,0};
+    int adcTickAmnt = 0;
+    int pingTickAmnt = 0;
+    double averageADC = 0;
+    int averagePing = 0;
+
+    double estimation = 0;
+    int pingVal = 0;
+    int adcVal = 0;
+    int i =low;
+    char buffer[200] = "";
+    for(i =low; i<high; i++){
+        servo_move(i);
+        adcVal = adc_read();
+//        estimation = 0.0000228813 * (adcVal * adcVal) - 0.0981288 * adcVal + 115.33455;
+        estimation = 4150000 * pow(adcVal, -1.64);
+        if(estimation < 25){
+            sprintf(buffer, "Object hit at: %.2f, at angle %d \r\n", estimation,i);
+                    uart_sendStr(buffer);
+            adcTickAmnt++;
+            averageADC += estimation;
+
+        }
+
+        pingVal = (((ping_read()/2)*.5)*34000)/16000000;
+        if(pingVal < 25){
+            pingTickAmnt++;
+            averagePing+= pingVal;
+        }
+
+        sprintf(buffer, "angle: %d scan: %.2f, average: %.2f, object count: %d \r\n",i,  estimation, averageADC / ((double)adcTickAmnt), adcTickAmnt);
+        uart_sendStr(buffer);
+
+
+
+    }
+
+    if(adcTickAmnt == 0 || pingTickAmnt == 0){
+        sprintf(buffer, "No objects found!");
+                uart_sendStr(buffer);
+        scanData.averageAdc = 100;
+        scanData.averagePing = 100;
+        return scanData;
+    }
+    averageADC = averageADC / ((double)(adcTickAmnt));
+    averagePing = averagePing/pingTickAmnt;
+    sprintf(buffer, "Final output:  average: %.2f, object count: %d \r\n", averageADC, adcTickAmnt);
+            uart_sendStr(buffer);
+    lcd_printf("%.2f, %d", averageADC, adcTickAmnt);
+    scanData.averageAdc = averageADC;
+    scanData.averagePing = averagePing;
+    if(adcTickAmnt > 0){
+        return scanData;
+    }
+    else{
+        scanData.averageAdc = 100;
+                scanData.averagePing = 100;
+                return scanData;
+    }
+
+}
+
+void avoidObject(oi_t *sensor_data)
+{
+    int objects_passed = 0; // bool
+
+    sensor_data->distance = 0;
+    double zero_y_distance = sensor_data->distance;
+    double zero_x_distance = sensor_data->distance;
+    double distance_traveled_x = 0;
+    double distance_traveled_y = 0;
+    int facing = POSITIVE_Y;
+
+    while(!objects_passed) {
+        scan_info distance_to_object = scan_cone(60, 120);
+        lcd_printf("average distance: %.2f", distance_to_object.averageAdc);
+        if (distance_to_object.averageAdc < 25) { // if close object in front of bot (facing positive y)
+            turn_counterclockwise(sensor_data, 90);
+            facing = NEGATIVE_X;
+            int found_object_to_left = 1; // assume object to the left
+            while (found_object_to_left) { //facing -x
+//                turn_counterclockwise(sensor_data, 90);
+//                facing = NEGATIVE_X; // turn back to the left   facing -x
+                distance_to_object = scan_cone(60, 120);
+                lcd_printf("average distance: %.2f", distance_to_object.averagePing);
+                if (distance_to_object.averagePing > 50) { // clear path to move
+                    found_object_to_left = 0;
+                    oi_getDistance(sensor_data); //call
+                    move_forward(sensor_data, 50);
+                    double distance_moved = oi_getDistance(sensor_data);
+                    distance_traveled_x -= distance_moved; //subtract bc moving on -x direction
+                    turn_clockwise(sensor_data, 90);
+                    facing = POSITIVE_Y;        // facing +y, break
+                }
+                else { // object   facing -x
+                    turn_counterclockwise(sensor_data, 90);
+                    facing = NEGATIVE_Y;
+                    oi_getDistance(sensor_data); //call
+                    move_forward(sensor_data, 50);
+                    double distance_moved = oi_getDistance(sensor_data);
+                    distance_traveled_y -= distance_moved; //subtract bc moving on -y direction
+                    turn_clockwise(sensor_data, 90);
+                    facing = NEGATIVE_X; // turn back to the left   facing -x
+                }
+                move_forward(sensor_data, -(distance_traveled_y) + 35);
+                turn_clockwise(sensor_data, 90);
+                facing = POSITIVE_X;
+                move_forward(sensor_data, -(distance_traveled_x));
+                turn_counterclockwise(sensor_data, 90);
+                facing = POSITIVE_Y;
+                while (1) {
+                    lcd_printf("success");
+                }
+            }
+
+        }
+    }
+}
 
 int main(void)
 {
@@ -113,75 +243,16 @@ int main(void)
 //
 //    }
     //lcd_printf("%.2f, %.2f", horizontalSpan, verticalSpan);
-        }}
+
+        }
+    }
     oi_free(sensor_data);
 
     return 0;
 }
 
-void avoidObject(oi_t *sensor_data)
-{
-    turn_counterclockwise(sensor_data, 90);
-    double adcValue = scan_cone(45, 135);
-
-
-    if (adcValue < 10)
-    {
-        avoidObject(sensor_data);
-    }
-    move_forward(sensor_data, 35);
-    turn_clockwise(sensor_data, 90);
-
-    adcValue = scan_cone(45, 135);
-    if (adcValue < 10)
-    {
-        avoidObject(sensor_data);
-    }
-    move_forward(sensor_data, 35 /*possibly, add the size of the pillar*/);
-    turn_clockwise(sensor_data, 90);
-    adcValue = scan_cone(45, 135);
-    if (adcValue < 10)
-    {
-        avoidObject(sensor_data);
-    }
-    move_forward(sensor_data, 35);
-    turn_counterclockwise(sensor_data, 90);
-
-    //drive forward
-
-}
-
-double scan_cone(int low, int high){
-    int objectTickAmnt = 0;
-    double averageValue = 0;
-    double estimation = 0;
-    int adcVal = 0;
-    int i =low;
-
-    for(i =low; i<high; i++){
-        servo_move(i);
-        adcVal = adc_read();
-//        estimation = 0.0000228813 * (adcVal * adcVal) - 0.0981288 * adcVal + 115.33455;
-        estimation = 4150000 * pow(adcVal, -1.64);
-        if(estimation < 25){
-            lcd_printf("object hit");
-            objectTickAmnt++;
-            averageValue += estimation;
-        }
 
 
 
-    }
-    if(objectTickAmnt ==0){
-        return 100;
-    }
-    averageValue= averageValue / ((double)(objectTickAmnt));
-    lcd_printf("%.2f, %d", averageValue, objectTickAmnt);
-    if(objectTickAmnt > 0){
-        return averageValue;
-    }
-    else{
-        return 100;
-    }
 
-}
+
