627109eddfaa5239041043393a92d97e0c0e735b
Rockey <rockey@iastate.edu>
2025-11-30 20:05:40 -0600

    refactor: move around functions to fix implicit declaration warnings

--- final_proj/main.c
+++ final_proj/main.c
@@ -32,11 +32,6 @@ typedef struct {
 
 } scan_info;
 
-void avoidObject(oi_t *sensor_data,  scan_info *scanData);
-void scan_cone(int low, int high, scan_info *scanData);
-void rotate_degrees(int angle, int direction, oi_t *sensor_data);
-void update_distance(double distance, int direction);
-void face_direction(int startDir, int finalDir /*0,1,2,3*/, oi_t *sensor_data);
 double horizontalPos = 0;
 double verticalPos = 0;
 #define POSITIVE_X 0
@@ -46,25 +41,6 @@ double verticalPos = 0;
 int directionGlobal = 0; //0,1,2,3 for NWSE
 
 int turnStatus = 0;
-void face_direction(int startDir, int finalDir /*0,1,2,3*/, oi_t *sensor_data){
-    int direction = 90*(startDir - finalDir);
-    if(direction <0){
-        direction+= 360;
-    }
-    else if(direction > 360){
-        direction -= 360;
-    }
-    if(direction > 180){
-
-        rotate_degrees(startDir, -1 * 90, sensor_data);
-    }
-    else if(direction <180){
-        rotate_degrees(startDir, 1 * 90, sensor_data);
-    }
-    else if(direction == 180){
-        rotate_degrees(startDir, 2 * 90, sensor_data);
-    }
-}
 void rotate_degrees(int angle, int direction, oi_t *sensor_data){ //CCW is positive
     int angleChange = direction/90;
     if(angleChange>0){
@@ -84,6 +60,26 @@ void rotate_degrees(int angle, int direction, oi_t *sensor_data){ //CCW is posit
     }
     directionGlobal = angle;
 }
+
+void face_direction(int startDir, int finalDir /*0,1,2,3*/, oi_t *sensor_data){
+    int direction = 90*(startDir - finalDir);
+    if(direction <0){
+        direction+= 360;
+    }
+    else if(direction > 360){
+        direction -= 360;
+    }
+    if(direction > 180){
+
+        rotate_degrees(startDir, -1 * 90, sensor_data);
+    }
+    else if(direction <180){
+        rotate_degrees(startDir, 1 * 90, sensor_data);
+    }
+    else if(direction == 180){
+        rotate_degrees(startDir, 2 * 90, sensor_data);
+    }
+}
 void update_distance(double distance, int direction ){
     if(direction == 0){
         horizontalPos+=distance;
@@ -98,6 +94,212 @@ void update_distance(double distance, int direction ){
             verticalPos-=distance;
         }
 }
+
+
+void scan_cone(int low, int high, scan_info *scanData){
+
+    int adcTickAmnt = 0;
+    int pingTickAmnt = 0;
+    double averageADC = 0;
+    int averagePing = 0;
+    double adcRad = 0;
+    double pingRad = 0;
+    double pingWidth = 0;
+    double adcWidth = 0;
+    double estimation = 0;
+    int pingVal = 0;
+    int adcVal = 0;
+    int i =low;
+    char buffer[200] = "";
+    for(i =low; i<high; i++){
+        servo_move(i);
+        adcVal = adc_read();
+//        estimation = 0.0000228813 * (adcVal * adcVal) - 0.0981288 * adcVal + 115.33455;
+        estimation = 4150000 * pow(adcVal, -1.64);
+        if(estimation < 25){
+            sprintf(buffer, "Object hit at: %.2f, at angle %d \r\n", estimation,i);
+ //                   uart_sendStr(buffer);
+            adcTickAmnt++;
+            averageADC += estimation;
+
+        }
+
+        pingVal = (((ping_read()/2)*.5)*34000)/16000000;
+        if(pingVal < 25){
+            pingTickAmnt++;
+            averagePing+= pingVal;
+        }
+
+        sprintf(buffer, "angle: %d scan: %.2f, avercage: %.2f, object count: %d \r\n",i,  estimation, averageADC / ((double)adcTickAmnt), adcTickAmnt);
+ //       uart_sendStr(buffer);
+
+
+
+    }
+
+    if(adcTickAmnt == 0 || pingTickAmnt == 0){
+        sprintf(buffer, "No objects found!");
+//                uart_sendStr(buffer);
+        scanData->averageAdc = 200;
+        scanData->averagePing = 200;
+        scanData->driveDist = 25+35;
+        return;
+    }
+    averageADC = averageADC / ((double)(adcTickAmnt));
+    averagePing = averagePing/pingTickAmnt;
+    sprintf(buffer, "Final output:  average: %.2f, object count: %d \r\n", averageADC, adcTickAmnt);
+//            uart_sendStr(buffer);
+    lcd_printf("%.2f, %d", averageADC, adcTickAmnt);
+    scanData->averageAdc = averageADC;
+    scanData->averagePing = averagePing;
+     adcRad = (adcTickAmnt) * (3.14 / 180.0);
+    adcWidth = 2 * averageADC * tan(adcRad / 2.0);
+
+    pingRad = (pingTickAmnt) * (3.14 / 180.0);
+    pingWidth = 2 *averagePing * tan(pingRad / 2.0);
+    scanData->adcWidth = adcWidth;
+    scanData->pingWidth = pingWidth;
+    if (adcTickAmnt > 0)
+    {
+        if (scanData->averageAdc < 10)
+        {
+            if (scanData->adcWidth > 7)   // check the values for bounds
+            {
+                scanData->driveDist = 17 + 35 + scanData->averageAdc;
+
+            }
+            else
+            {
+                scanData->driveDist = 35 + 11 + scanData->averageAdc;
+            }
+
+        }
+        else
+        {
+            if (scanData->adcWidth > 11)   // check the values for bounds
+            {
+                scanData->driveDist = 17 + 35 + scanData->averageAdc;
+            }
+            else if (  scanData->adcWidth > 6)
+            {
+                scanData->driveDist = 35 + 11 + scanData->averageAdc;
+            }
+            else if (scanData->adcWidth > 0)
+            {
+                scanData->driveDist = 35 + 5 + scanData->averageAdc;
+            }
+        }
+
+
+
+    }
+    else
+    {
+        scanData->driveDist = 200;
+    }
+
+    sprintf(buffer, " drive distance %.2f", scanData->driveDist);
+//    uart_sendStr(buffer);
+    lcd_printf("adc width: %.2f\r\n ping width: %/2f\r\n", adcWidth, pingWidth);
+    if(adcTickAmnt > 0){
+        return;
+    }
+    else{
+        scanData->averageAdc = 100;
+                scanData->averagePing = 100;
+                return;
+    }
+
+}
+
+void avoidObject(oi_t *sensor_data, scan_info *scanData)
+{
+
+    char buffer[50];
+    //update_distance(driveDist, direction);
+    rotate_degrees(directionGlobal, 90, sensor_data);
+
+
+    scan_cone(45, 135, scanData);
+
+
+    sprintf(buffer, "adcValue in recursion: %.2f", scanData->averageAdc);
+    uart_sendStr(buffer);
+    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist/2)
+    {
+        avoidObject(sensor_data, scanData);
+    }
+  //  move_forward(sensor_data, scanData->driveDist);
+    move_forward(sensor_data, scanData->driveDist - scanData->averageAdc); //sideways
+    update_distance(scanData->driveDist, directionGlobal);
+    rotate_degrees(directionGlobal, -90, sensor_data);
+
+
+    scan_cone(45, 135, scanData);
+    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist)
+    {
+        avoidObject(sensor_data, scanData);
+    }
+    move_forward(sensor_data, scanData->driveDist ); //straight
+    update_distance(scanData->driveDist, directionGlobal);
+    rotate_degrees(directionGlobal, -90, sensor_data);
+
+    scan_cone(45, 135, scanData);
+    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist/2)
+    {
+        avoidObject(sensor_data, scanData);
+    }
+
+
+
+
+    if (!turnStatus)
+    {
+
+        face_direction(directionGlobal, NEGATIVE_X, sensor_data);
+    }
+    else
+    {
+
+        face_direction(directionGlobal, NEGATIVE_Y, sensor_data);
+    }
+    while (1)
+    {
+        scan_cone(45, 135, scanData);
+        if (scanData->averageAdc < 25)
+        {
+            avoidObject(sensor_data, scanData);
+        }
+        else
+        {
+            if (move_forward(sensor_data, 25) == BOUNDARY)
+            {
+                break;
+            }
+        }
+
+    }
+    //move_forward(sensor_data, scanData->driveDist); //move TO THE WHITE TAPE
+    //update_distance(scanData->driveDist, direction);
+
+
+
+
+
+//    }
+
+
+
+
+
+
+
+
+
+    //drive forward
+
+}
+
 int main(void)
 {
 
@@ -216,208 +418,4 @@ int main(void)
     oi_free(sensor_data);
 
     return 0;
-}
-
-void avoidObject(oi_t *sensor_data, scan_info *scanData)
-{
-
-    char buffer[50];
-    //update_distance(driveDist, direction);
-    rotate_degrees(directionGlobal, 90, sensor_data);
-
-
-    scan_cone(45, 135, scanData);
-
-
-    sprintf(buffer, "adcValue in recursion: %.2f", scanData->averageAdc);
-    uart_sendStr(buffer);
-    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist/2)
-    {
-        avoidObject(sensor_data, scanData);
-    }
-  //  move_forward(sensor_data, scanData->driveDist);
-    move_forward(sensor_data, scanData->driveDist - scanData->averageAdc); //sideways
-    update_distance(scanData->driveDist, directionGlobal);
-    rotate_degrees(directionGlobal, -90, sensor_data);
-
-
-    scan_cone(45, 135, scanData);
-    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist)
-    {
-        avoidObject(sensor_data, scanData);
-    }
-    move_forward(sensor_data, scanData->driveDist ); //straight
-    update_distance(scanData->driveDist, directionGlobal);
-    rotate_degrees(directionGlobal, -90, sensor_data);
-
-    scan_cone(45, 135, scanData);
-    if (scanData->averageAdc < 25 || scanData->averageAdc <= scanData->driveDist/2)
-    {
-        avoidObject(sensor_data, scanData);
-    }
-
-
-
-
-    if (!turnStatus)
-    {
-
-        face_direction(directionGlobal, NEGATIVE_X, sensor_data);
-    }
-    else
-    {
-
-        face_direction(directionGlobal, NEGATIVE_Y, sensor_data);
-    }
-    while (1)
-    {
-        scan_cone(45, 135, sensor_data);
-        if (scanData->averageAdc < 25)
-        {
-            avoidObject(sensor_data, scanData);
-        }
-        else
-        {
-            if (move_forward(sensor_data, 25) == BOUNDARY)
-            {
-                break;
-            }
-        }
-
-    }
-    //move_forward(sensor_data, scanData->driveDist); //move TO THE WHITE TAPE
-    //update_distance(scanData->driveDist, direction);
-
-
-
-
-
-//    }
-
-
-
-
-
-
-
-
-
-    //drive forward
-
-}
-
-void scan_cone(int low, int high, scan_info *scanData){
-
-    int adcTickAmnt = 0;
-    int pingTickAmnt = 0;
-    double averageADC = 0;
-    int averagePing = 0;
-    double adcRad = 0;
-    double pingRad = 0;
-    double pingWidth = 0;
-    double adcWidth = 0;
-    double estimation = 0;
-    int pingVal = 0;
-    int adcVal = 0;
-    int i =low;
-    char buffer[200] = "";
-    for(i =low; i<high; i++){
-        servo_move(i);
-        adcVal = adc_read();
-//        estimation = 0.0000228813 * (adcVal * adcVal) - 0.0981288 * adcVal + 115.33455;
-        estimation = 4150000 * pow(adcVal, -1.64);
-        if(estimation < 25){
-            sprintf(buffer, "Object hit at: %.2f, at angle %d \r\n", estimation,i);
- //                   uart_sendStr(buffer);
-            adcTickAmnt++;
-            averageADC += estimation;
-
-        }
-
-        pingVal = (((ping_read()/2)*.5)*34000)/16000000;
-        if(pingVal < 25){
-            pingTickAmnt++;
-            averagePing+= pingVal;
-        }
-
-        sprintf(buffer, "angle: %d scan: %.2f, avercage: %.2f, object count: %d \r\n",i,  estimation, averageADC / ((double)adcTickAmnt), adcTickAmnt);
- //       uart_sendStr(buffer);
-
-
-
-    }
-
-    if(adcTickAmnt == 0 || pingTickAmnt == 0){
-        sprintf(buffer, "No objects found!");
-//                uart_sendStr(buffer);
-        scanData->averageAdc = 200;
-        scanData->averagePing = 200;
-        scanData->driveDist = 25+35;
-        return;
-    }
-    averageADC = averageADC / ((double)(adcTickAmnt));
-    averagePing = averagePing/pingTickAmnt;
-    sprintf(buffer, "Final output:  average: %.2f, object count: %d \r\n", averageADC, adcTickAmnt);
-//            uart_sendStr(buffer);
-    lcd_printf("%.2f, %d", averageADC, adcTickAmnt);
-    scanData->averageAdc = averageADC;
-    scanData->averagePing = averagePing;
-     adcRad = (adcTickAmnt) * (3.14 / 180.0);
-    adcWidth = 2 * averageADC * tan(adcRad / 2.0);
-
-    pingRad = (pingTickAmnt) * (3.14 / 180.0);
-    pingWidth = 2 *averagePing * tan(pingRad / 2.0);
-    scanData->adcWidth = adcWidth;
-    scanData->pingWidth = pingWidth;
-    if (adcTickAmnt > 0)
-    {
-        if (scanData->averageAdc < 10)
-        {
-            if (scanData->adcWidth > 7)   // check the values for bounds
-            {
-                scanData->driveDist = 17 + 35 + scanData->averageAdc;
-
-            }
-            else
-            {
-                scanData->driveDist = 35 + 11 + scanData->averageAdc;
-            }
-
-        }
-        else
-        {
-            if (scanData->adcWidth > 11)   // check the values for bounds
-            {
-                scanData->driveDist = 17 + 35 + scanData->averageAdc;
-            }
-            else if (  scanData->adcWidth > 6)
-            {
-                scanData->driveDist = 35 + 11 + scanData->averageAdc;
-            }
-            else if (scanData->adcWidth > 0)
-            {
-                scanData->driveDist = 35 + 5 + scanData->averageAdc;
-            }
-        }
-
-
-
-    }
-    else
-    {
-        scanData->driveDist = 200;
-    }
-
-    sprintf(buffer, " drive distance %.2f", scanData->driveDist);
-//    uart_sendStr(buffer);
-    lcd_printf("adc width: %.2f\r\n ping width: %/2f\r\n", adcWidth, pingWidth);
-    if(adcTickAmnt > 0){
-        return;
-    }
-    else{
-        scanData->averageAdc = 100;
-                scanData->averagePing = 100;
-                return;
-    }
-
 }
